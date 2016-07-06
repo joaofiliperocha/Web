95c276b7b38802c3849ae9353a230ff4b1bd3888
João Rocha <jfsllrocha@gmail.com>
2016-07-06 11:20:47 +0100

    Add files via upload

---
+++ SaveToDisk/SaveFile.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="SaveFile.aspx.cs" Inherits="Playground.SaveFile" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <h2>Create a Hello File !!!</h2>
+    <form role="form" runat="server">
+        <div class="form-group">
+            <label class="control-label" for="exampleInputEmail">Text to Save:</label>
+            <asp:TextBox ID="textToSave" runat="server" CssClass="form-control" placeholder="write some text"></asp:TextBox>
+        </div>
+        <div>
+            <br />
+            <asp:LinkButton ID="SaveBtn" runat="server" CssClass="btn btn-primary" Text="[ SAVE to Disk ]" OnClick="SaveBtn_Click"></asp:LinkButton>
+        </div>
+    </form>
+</body>
+</html>
---
+++ SaveToDisk/SaveFile.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Playground
+{
+    public partial class SaveFile : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void SaveBtn_Click(object sender, EventArgs e)
+        {
+            MemoryStream ms = new MemoryStream();
+            string msg = "Hello";
+            ms.Write(Encoding.UTF8.GetBytes(msg), 0, msg.Length);
+            SaveAsPopup(ms, "text/plain", "Hello.txt");
+        }
+
+        internal void SaveAsPopup(MemoryStream stm, string mimeType, string fileName)
+        {
+            this.Response.Buffer = true;
+            this.Response.Clear();
+            this.Response.ContentType = mimeType;
+            this.Response.AddHeader("content-disposition", "attachment; filename=" + fileName);
+            stm.WriteTo(this.Response.OutputStream);
+            this.Response.Flush();
+            this.Response.End();
+        }
+    }
+}
